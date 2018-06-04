4d6c6c98cc22f5cc81564badd0e5fd5f8116ab47
Luke Harmon <lukeharmon@Lukes-MacBook-Pro-2.local>
2018-06-04 10:47:20 -0700

    add working (?) discrete correlation

--- R/discreteCorrelation.R
+++ R/discreteCorrelation.R
@@ -1,50 +1,58 @@
-discreteCorrelation<-function(phy, charA, charB, modelType="ER") {
-	
-	# check for perfect name matching
-	# this could be improved a lot
-	
-	td1<-name.check(phy, charA)
-	td2<-name.check(phy, charB)
 
-	if(td1 != "OK" | td2 != "OK") stop("Names don't match")
-	
-	# this changes the discrete data to 1:n and remembers the original charStates
-	datA<-as.factor(charA)
-	charStatesA<-levels(datA)
-	kA<-nlevels(datA)
-	
-	datB<-as.factor(charB)
-	charStatesB<-levels(datB)
-	kB<-nlevels(datB)
-	
-	if(kA != 2 | kB != 2) stop("Only 2-state characters currently supported")
-		
-	mergedDat<-combineDiscreteCharacters(datA, datB)
-			
-	charStates<-levels(mergedDat)
-	k<-nlevels(mergedDat)
-		
-	ndat<-as.numeric(mergedDat)
-	names(ndat)<-names(mergedDat)
-		
-	constraint<-makeDiscreteCorrelationConstraints(modelType=modelType)
-	
-	lik<-make.mkn(phy, ndat, k=k)
-	ulik<-constrain(lik, formulae=constraint$uCon, extra=constraint$uExtra)
-	clik<-constrain(lik, formulae=constraint$cCon, extra=constraint$cExtra)
-	
-	unames<-argnames(ulik)
-	uML<-find.mle(ulik, setNames(rep(1,length(unames)), argnames(ulik)))
-	
-	cnames<-argnames(clik)
-	cML<-find.mle(clik, setNames(rep(1,length(cnames)), argnames(clik)))
-	
-	lrStat<-2*(cML$lnLik - uML$lnLik)
-	lrDF <- 2*(length(cnames)-length(unames))
-	
-	lrPVal <- pchisq(lrStat, lrDF, lower.tail=F)
-	
-	return(list(unames=unames, cnames=cnames, lrStat= lrStat, lrDF= lrDF, lrPVal= lrPVal))
+#' Test for an evolutionary correlation between two continuous characters
+#'
+#' @param tree A phylogenetic tree
+#' @param table A data matrix
+#' @param column1 One column from table; must be a binary discrete trait
+#' @param column2 Second column from table; must be a binary discrete trait
+#' @param modelType Discrete model, one of "ER", "SYM", "ARD"
+#' @export
+discreteCorrelation<-function(tree, table, column1, column2, modelType="ER") {
+
+  # check for perfect name matching
+  # this could be improved a lot
+
+  td <- make.treedata(tree, table)
+
+  td1 <- select(td, column1, column2)
+
+  # this changes the discrete data to 1:n and remembers the original charStates
+  datA<-as.factor(td1[[column1]])
+  charStatesA<-levels(datA)
+  kA<-nlevels(datA)
+
+  datB<-as.factor(td1[[column2]])
+  charStatesB<-levels(datB)
+  kB<-nlevels(datB)
+
+  if(kA != 2 | kB != 2) stop("Only 2-state characters currently supported")
+
+  mergedDat<-combineDiscreteCharacters(datA, datB)
+
+  charStates<-levels(mergedDat)
+  k<-nlevels(mergedDat)
+
+  ndat<-as.numeric(mergedDat)
+  names(ndat)<-names(mergedDat)
+
+  constraint<-makeDiscreteCorrelationConstraints(modelType=modelType)
+
+  lik<-make.mkn(phy, ndat, k=k)
+  ulik<-constrain(lik, formulae=constraint$uCon, extra=constraint$uExtra)
+  clik<-constrain(lik, formulae=constraint$cCon, extra=constraint$cExtra)
+
+  unames<-argnames(ulik)
+  uML<-find.mle(ulik, setNames(rep(1,length(unames)), argnames(ulik)))
+
+  cnames<-argnames(clik)
+  cML<-find.mle(clik, setNames(rep(1,length(cnames)), argnames(clik)))
+
+  lrStat<-2*(cML$lnLik - uML$lnLik)
+  lrDF <- 2*(length(cnames)-length(unames))
+
+  lrPVal <- pchisq(lrStat, lrDF, lower.tail=F)
+
+  return(list(unames=unames, cnames=cnames, lrStat= lrStat, lrDF= lrDF, lrPVal= lrPVal))
 
 }
 
