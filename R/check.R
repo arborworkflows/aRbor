890cb05ac1abf3136045b4d23eef486c04e01dae
Luke Harmon <lukeharmon@Lukes-MacBook-Pro-2.local>
2018-06-05 10:25:22 -0700

    update checkfactor to deal with logicals; up version

--- R/check.R
+++ R/check.R
@@ -36,38 +36,58 @@ checkNumeric <- function(tdObject, return.numeric=TRUE) {
 #' @examples
 #' data(anolis)
 #' @export
-checkFactor <- function(tdObject, return.factor=TRUE) {
+checkFactor <- function (tdObject, return.factor = TRUE)
+{
   classes <- sapply(tdObject$dat, class)
-  valid <- which(classes=="factor")
-  if(length(valid) < ncol(tdObject$dat)){
-    #Which data are numeric
+  valid <- which(classes == "factor")
+  if (length(valid) < ncol(tdObject$dat)) {
     are.numeric <- which(classes %in% c("numeric", "integer"))
-    if(length(are.numeric) > 0){
-      warning("Data contain numeric entries, which will be converted to factors for discrete ancestral state reconstruction")
-      #convert them to factors
-      tdObject$dat[, are.numeric] <- lapply(as.data.frame(tdObject$dat[,are.numeric]), factor)
-      ##Check to see if converted data has any columns that appear continuous
+    if (length(are.numeric) > 0) {
+      warning("Data contain numeric entries, which will be converted to factors for discrete analysis")
+      tdObject$dat[, are.numeric] <- lapply(as.data.frame(tdObject$dat[,
+                                                                       are.numeric]), factor)
       classes <- sapply(tdObject$dat, class)
-      valid <- which(classes=="factor")
-      too.many.levels <- which(sapply(tdObject$dat,function(x) length(unique(x)))==nrow(tdObject$dat))
-      if(length(too.many.levels) > 0){
-        warning(paste("Conversion failed for data columns", paste(colnames(tdObject$dat)[too.many.levels], collapse=" "), "as these data have no shared states. These data will be removed", sep=" "))
+      valid <- which(classes == "factor")
+      too.many.levels <- which(sapply(tdObject$dat, function(x) length(unique(x))) ==
+                                 nrow(tdObject$dat))
+      if (length(too.many.levels) > 0) {
+        warning(paste("Conversion failed for data columns",
+                      paste(colnames(tdObject$dat)[too.many.levels],
+                            collapse = " "), "as these data have no shared states. These data will be removed",
+                      sep = " "))
         valid <- valid[which(!(valid %in% too.many.levels))]
       }
     }
-
-    if(length(valid)==0){
+    are.logical <- which(classes == "logical")
+    if (length(are.logical) > 0) {
+        warning("Data contain logical (T,F) entries, which will be converted to factors for discrete analysis")
+        tdObject$dat[, are.logical] <- lapply(as.data.frame(tdObject$dat[, are.logical]), factor)
+        classes <- sapply(tdObject$dat, class)
+        valid <- which(classes == "factor")
+        too.many.levels <- which(sapply(tdObject$dat, function(x) length(unique(x))) ==
+                                   nrow(tdObject$dat))
+        if (length(too.many.levels) > 0) {
+          warning(paste("Conversion failed for data columns",
+                        paste(colnames(tdObject$dat)[too.many.levels],
+                              collapse = " "), "as these data have no shared states. These data will be removed",
+                        sep = " "))
+          valid <- valid[which(!(valid %in% too.many.levels))]
+        }
+    }
+    if (length(valid) == 0) {
       stop("Data does not contain any data that can be used for discrete ancestral state reconstruction")
-    } else {
+    }
+    else {
       tdObject$dat <- select(tdObject$dat, valid)
     }
   }
-  if(return.factor){
-    for(i in 1:ncol(tdObject$dat)){
+  if (return.factor) {
+    for (i in 1:ncol(tdObject$dat)) {
       tdObject$dat[[i]] <- factor(tdObject$dat[[i]])
     }
     return(tdObject)
-  } else {
+  }
+  else {
     invisible()
   }
 }
