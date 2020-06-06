2dbbe0a50daa76929a7a70f04db392251d447b5e
Luke Harmon <lukeh@uidaho.edu>
2020-06-05 18:06:48 -0700

    fix import

--- R/aRbor.R
+++ R/aRbor.R
@@ -1,12 +1,12 @@
 #' aRbor: a package to interact with Arbor Workflows
-#' 
-#' The aRbor package is a set of functions that can be integrated as workflow modules in the 
+#'
+#' The aRbor package is a set of functions that can be integrated as workflow modules in the
 #' Arbotr webapp. aRbor also includes the specifications of the "treedata" object.
-#' 
+#'
 #' @docType package
 #' @author Josef Uyeda and Luke Harmon
 #' @name aRbor
-#' @import ape geiger picante phytools dplyr RColorBrewer
+#' @import ape geiger picante phytools dplyr RColorBrewer treeplyr
 #' @importFrom lazyeval all_dots
 #' @importFrom diversitree make.mkn constrain argnames find.mle make.bisse starting.point.bisse asr.joint asr.marginal make.prior.exponential mcmc asr.stoch
 #' @importFrom nlme gls
